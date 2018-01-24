6798f1a7665b4e4e724e465042e09d418b7d59c8
Nate Clark <natec425@gmail.com>
2018-01-24 12:41:12 -0600

    draft header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,50 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
+        style={{
+            background: '#dddddd',
+            marginBottom: '1.45rem',
+            paddingBottom: 5
+        }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
+        <img
+            style={{
+                margin: 0,
+                marginTop: 10,
+                marginLeft: 15,
+                marginRight: 0,
+                borderRadius: '50%',
+                height: 100,
+                display: 'inline'
+            }}
+            src="/static/images/me.jpg"
+        />
+        <div
+            style={{
+                margin: '0 auto',
+                maxWidth: 960,
+                padding: '1.45rem 1.0875rem',
+                display: 'inline'
+            }}
         >
-          Gatsby
-        </Link>
-      </h1>
+            <h1
+                style={{
+                    margin: 0,
+                    display: 'inline'
+                }}
+            >
+                <Link to="/">Nate Clark</Link>
+            </h1>
+            <h3 style={{ margin: 0, paddingLeft: '1.5rem', display: 'inline' }}>
+                Software Developer
+            </h3>
+            <h3 style={{ margin: 0, paddingLeft: '1.5rem', display: 'inline' }}>
+                Educator
+            </h3>
+        </div>
     </div>
-  </div>
-)
+);
 
-export default Header
+export default Header;
