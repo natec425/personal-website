ea3d91f6df1f1a59db445d4363b955e7b285c891
Nate Clark <natec425@gmail.com>
2018-01-24 15:25:11 -0600

    center body

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -11,7 +11,7 @@ const TemplateWrapper = ({ children }) => (
         <Header />
         <div
             style={{
-                margin: '0 5%',
+                margin: '0 auto',
                 maxWidth: 1028,
                 padding: '0px 1.0875rem 1.45rem',
                 paddingTop: 0
