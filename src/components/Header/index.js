4d92b9ba3ec4677d62473215df0c1e0f3a9f289a
Nate Clark <natec425@gmail.com>
2018-03-29 12:35:34 -0500

    Refactor Header styles into object

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,39 +2,36 @@ import React from 'react';
 import Link from 'gatsby-link';
 import me_img from '../../../images/me.jpg';
 
+const styles = {
+    headerContainer: {
+        background: '#dddddd',
+        marginBottom: '1.45rem',
+        paddingLeft: '15%',
+        paddingRight: '5%',
+        paddingBottom: 5,
+        paddingTop: 15
+    },
+    headerMainSection: {
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '1.45rem 1.0875rem',
+        display: 'inline'
+    },
+    nameLinkContainer: {
+        margin: 0,
+        display: 'inline'
+    },
+    titleContainer: { margin: 0, paddingLeft: '1.5rem', display: 'inline' }
+};
+
 const Header = () => (
-    <div
-        style={{
-            background: '#dddddd',
-            marginBottom: '1.45rem',
-            paddingLeft: '15%',
-            paddingRight: '5%',
-            paddingBottom: 5,
-            paddingTop: 15
-        }}
-    >
-        <div
-            style={{
-                margin: '0 auto',
-                maxWidth: 960,
-                padding: '1.45rem 1.0875rem',
-                display: 'inline'
-            }}
-        >
-            <h1
-                style={{
-                    margin: 0,
-                    display: 'inline'
-                }}
-            >
+    <div style={styles.headerContainer}>
+        <div style={styles.headerMainSection}>
+            <h1 style={styles.nameLinkContainer}>
                 <Link to="/">Nate Clark</Link>
             </h1>
-            <h3 style={{ margin: 0, paddingLeft: '1.5rem', display: 'inline' }}>
-                Software Developer
-            </h3>
-            <h3 style={{ margin: 0, paddingLeft: '1.5rem', display: 'inline' }}>
-                Educator
-            </h3>
+            <h3 style={styles.titleContainer}>Software Developer</h3>
+            <h3 style={styles.titleContainer}>Educator</h3>
         </div>
     </div>
 );
