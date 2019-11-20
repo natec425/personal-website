a81de0a4c32a901529f0ba2066612307cf98f676
Nate Clark <natec425@gmail.com>
2019-11-20 10:10:01 -0600

    Add favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,6 +4,9 @@ import Helmet from "react-helmet";
 
 import Header from "../components/Header";
 import "./index.css";
+import appleTouchIcon from "../../images/apple-touch-icon.png";
+import favicon32x32 from "../../images/favicon-32x32.png";
+import favicon16x16 from "../../images/favicon-16x16.png";
 
 const TemplateWrapper = ({ children }) => (
   <div>
@@ -13,6 +16,9 @@ const TemplateWrapper = ({ children }) => (
         name="Description"
         content="Nate Clark's home page. Nate is a Software Developer and Educator in Water Valley, MS."
       />
+      <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+      <link rel="icon" type="image/png" sizes="32x32" href={favicon32x32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={favicon16x16} />
     </Helmet>
     <Header />
     <div
